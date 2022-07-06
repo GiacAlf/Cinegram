89879a17e1bcbcb5461849763ec7b27e69b8f055
giacomo.alfani@student.univaq.it <giacomo.alfani@student.univaq.it>
2022-07-06 22:55:13 +0200

    Commit 59: riscritto CfrontController in italiano

--- Controller/CFrontController.php
+++ Controller/CFrontController.php
@@ -2,54 +2,56 @@
 
 class CFrontController {
 
-    public function run(): void{
-        $path = $this->parsingFrontControllerUrl() ;
-        $arraypath = explode("/","$path");
-        array_shift($arraypath);
-        $controller = "C". ucfirst($arraypath[0]);
-        array_shift($arraypath);
-        $method = $this->capisciUrl($arraypath[0]);
-        if ( class_exists( $controller ) ) {
-            if ( method_exists($controller, $method ) ) {
+    public function run(): void {
+
+        $path = $this->parsingFrontControllerUrl();
+        $arrayPath = explode("/","$path");
+        array_shift($arrayPath);
+        $controller = "C". ucfirst($arrayPath[0]);
+        array_shift($arrayPath);
+        $method = $this->capisciUrl($arrayPath[0]);
+        if (class_exists($controller)) {
+            if (method_exists($controller, $method)) {
                 $real_controller = new $controller();
             }
             else {
                 $view = new VErrore();
-                $view ->error(3);
+                $view->error(3);
                 return;
-
             }
         }
         else {
             $view = new VErrore();
-            $view ->error(2);
+            $view->error(2);
             return;
         }
-        array_shift($arraypath);
-        switch (count($arraypath)){
+        array_shift($arrayPath);
+        switch (count($arrayPath)) {
             case (0):
                 $real_controller->$method();
                 break;
             case (1):
-                $real_controller->$method( $arraypath[0] );
+                $real_controller->$method($arrayPath[0]);
                 break;
             case (2):
-                $real_controller->$method( $arraypath[0] ,$arraypath[1] );
+                $real_controller->$method($arrayPath[0], $arrayPath[1]);
                 break;
         }
     }
 
 
-    private function capisciUrl(?string $path): ?string{
-        $arrayRisultato = explode("-",$path);
+    private function capisciUrl(?string $path): ?string {
+
+        $arrayRisultato = explode("-", $path);
         $arrayRisultato[1] = ucfirst($arrayRisultato[1]);
-        return ($arrayRisultato[0].$arrayRisultato[1]);
+        return ($arrayRisultato[0] . $arrayRisultato[1]);
     }
 
-    private function parsingFrontControllerUrl():string{
+
+    private function parsingFrontControllerUrl(): string {
+
         $url = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
         $url_base = $GLOBALS["URLBASE"];
-        $frontUrl = str_replace($url_base , "", $url);
-        return $frontUrl;
+        return str_replace($url_base, "", $url);
     }
 }
