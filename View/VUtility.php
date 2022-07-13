486b49b272dc0f679e1d254d6fb0dc2edabe477c
DamRos00 <rossetti624@gmail.com>
2022-07-13 13:26:38 +0200

    Commit: altro php doc

--- View/VUtility.php
+++ View/VUtility.php
@@ -1,22 +1,31 @@
 <?php
 
 /**
- *Classe di utility: restituisce informazioni necessarie
+ * Classe di utility: restituisce informazioni necessarie
  * a tutte le altre classi view
  */
 class VUtility {
 
+    /**
+     * Metodo che restituisce il nome del server che ospita l'applicazione
+     * @return string|null
+     */
     public static function getHttpHost(): ?string {
         return $_SERVER['HTTP_HOST'];
     }
 
-
+    /**
+     * Metodo che restituisce l'URL cliccata dall'utente, dopo
+     * la parte HOST dell'URL
+     * @return string|null
+     */
     public static function getRequestUri(): ?string {
         return $_SERVER['REQUEST_URI'];
     }
 
     /**
-     * Metodo che restituisce il nome del server che ospita l'applicazione
+     * Metodo che restituisce il nome del server che ospita l'applicazione,
+     * utile a comporre l'URL
      * @return string|null
      */
     public static function getRootDir(): ?string {
